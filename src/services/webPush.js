0ff1f1b891e0d480c6f08237d90d31ae84e403db
nicolas <lopez.nicolas.gabriel09@gmail.com>
2023-03-06 16:08:29 -0300

    actualizacion deploy

--- src/services/webPush.js
+++
@@ -1,9 +0,0 @@
-const webpush = require('web-push'); 
-
-webpush.setVapidDetails(
-    `mailto:${process.env.MAIL_ADMIN_ADRESS}`,
-     process.env.PUBLIC_VAPID_KEY,
-     process.env.PRIVATE_VAPID_KEY
-);
-
-module.exports = webpush;
