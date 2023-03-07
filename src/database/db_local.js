727906596a5e188fdc42186b53093ed5bcd3eaac
nicolas <lopez.nicolas.gabriel09@gmail.com>
2023-03-07 15:35:21 -0300

    actualizacion bbdd deploy

--- src/database/db_local.js
+++ src/database/db_local.js
@@ -17,16 +17,14 @@ if(process.env.NODE_ENV !== 'production'){
     console.log('production');
     console.log(process.env.NODE_ENV);
     const url = encodeURIComponent(process.env.DATABASE_URL);
-     connection = mysql.createConnection(process.env.DATABASE_URL//'mysql://d34k184vnc4rp3hhhae8:pscale_pw_v4UreiQWLPaNAD3x4o32SHUzougN73MEcegjxRFLxwT@aws-sa-east-1.connect.psdb.cloud/red_social?ssl={"rejectUnauthorized":true}'
+     connection = mysql.createConnection(//url//'mysql://d34k184vnc4rp3hhhae8:pscale_pw_v4UreiQWLPaNAD3x4o32SHUzougN73MEcegjxRFLxwT@aws-sa-east-1.connect.psdb.cloud/red_social?ssl={"rejectUnauthorized":true}'
 
-       /* {host : 'aws-sa-east-1.connect.psdb.cloud',
+         {host : 'sql10.freemysqlhosting.net',
           port: '3306',
-          user :'u3uegk6ognbdbrtmm7ar',
-          password :'pscale_pw_2GtCjWnHWKQtDNn7sSSRXtjLewYHyeid3oel1yV1mJe',
-          database : 'red_social',
-          dialect: 'mysql',
-          dialectModule : mysql2,
-          operatorsAliases: 0,}*/
+          user :'sql10603711',
+          password :'1631RrTnPI',
+          database : 'red_social',}
+          
     );
 }
 
